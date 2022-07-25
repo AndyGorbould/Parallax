14479180d7306af6c1946b32342190dd0c62a447
andy <gorbould@mail.com>
2022-07-25 16:57:37 +0200

    timing waaaay out

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ wrapper.addEventListener("scroll", () => {
   console.log(scrolled);
 });
 
-title = document.querySelector(".title");
+title = document.querySelector(".title3");
 
 title.addEventListener("click", () => {
   anime({
     targets: ".title",
     color: "rgba(255, 242, 0, 0)",
-    scale: 0.4,
-    duration: 5000,
+    translateZ: [100, 0],
+    duration: 2016,
     loop: true,
   });
   anime({
@@ -24,14 +24,16 @@ title.addEventListener("click", () => {
     color: "rgba(197, 46, 0, 1)",
     scale: 0.8,
     translateY: [0, 100],
-    delay: 500,
+    duration: 1008,
+    delay: 1008,
     direction: "alternate",
     loop: true,
   });
   anime({
     targets: ".title3",
     translateZ: [0, 100],
-    delay: 50,
+    duration: 2016,
+    delay: 504,
     direction: "alternate",
     loop: true,
   });
