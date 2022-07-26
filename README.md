dc16b5393102ff1362c65a0406a5aa2b9d766532
andy <gorbould@mail.com>
2022-07-26 10:44:43 +0200

    scroll listener working now

--- README.md
+++ README.md
@@ -7,5 +7,6 @@ Ironically, this project is far lower on the [Scoville Scale](https://en.wikiped
 
 Image credits: [CryptoGene-3D](https://cryptogene.itch.io/)
 
-Add js listener for scroll, fade out & scale up the title
+Add js listener for scroll, fade out & scale up the title - this doesn't work as well as expected, due to everything being in a 'wrapper' div so I cannot find the 'window.scrollY' values - fixed by getBoundingClientRect() on the farthest image in the wrapper (background)
+
 Add la cucaracha to catch ;)
--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-// const anime = require('./anime.min.js');
-
-wrapper = document.querySelector(".wrapper");
+let wrapper = document.querySelector(".wrapper");
 
 wrapper.addEventListener("scroll", () => {
   console.log("keep scrollin' scrollin' scrollin'");
 
-  let scrolled = window.wrapper.scrollY;
-  console.log(scrolled);
+  let bg = document.querySelector(".bg");
+  var offsets = bg.getBoundingClientRect();
+  var top = offsets.y;
+  console.log(top);
+  previousPosition = top;
 });
 
 title = document.querySelector(".title3");
 
-title.addEventListener("click", () => {
-  anime({
-    targets: ".title",
-    color: "rgba(255, 242, 0, 0)",
-    translateZ: [100, 0],
-    duration: 2016,
-    loop: true,
-  });
-  anime({
-    targets: ".title2",
-    color: "rgba(197, 46, 0, 1)",
-    scale: 0.8,
-    translateY: [0, 100],
-    duration: 1008,
-    delay: 1008,
-    direction: "alternate",
-    loop: true,
-  });
-  anime({
-    targets: ".title3",
-    translateZ: [0, 100],
-    duration: 2016,
-    delay: 504,
-    direction: "alternate",
-    loop: true,
-  });
-});
+// title.addEventListener("click", () => {
+//   anime({
+//     targets: ".title",
+//     color: "rgba(255, 242, 0, 0)",
+//     translateZ: [100, 0],
+//     duration: 2016,
+//     loop: true,
+//   });
+//   anime({
+//     targets: ".title2",
+//     color: "rgba(197, 46, 0, 1)",
+//     scale: 0.8,
+//     translateY: [0, 100],
+//     duration: 1008,
+//     delay: 1008,
+//     direction: "alternate",
+//     loop: true,
+//   });
+//   anime({
+//     targets: ".title3",
+//     translateZ: [0, 100],
+//     duration: 2016,
+//     delay: 504,
+//     direction: "alternate",
+//     loop: true,
+//   });
+// });
